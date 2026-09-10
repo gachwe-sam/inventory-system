51af03b94b0c559e2627900ebbb01583b46e693b
gachwe-sam <mainagachwe@gmail.com>
2026-09-10 16:26:32 +0300

    search bar for suppliers succefully integrated

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -1,13 +1,21 @@
 @extends('layouts.adminlte')
 
-
 @section('content')
 <x-ui.page-header title="Suppliers" actionLabel="Add Supplier" :actionRoute="route('suppliers.create')" />
-<x-ui.search-bar name="search" :value="request('search')" placeholder="Search name or description" />
+
+<form method="GET" action="{{ route('suppliers.index') }}" class="row g-2 my-3">
+    <x-ui.search-bar name="search" :value="request('search')" placeholder="Search name or email" target="suppliers-table" />
+
+    <div class="col-auto">
+        <button type="submit" class="btn btn-primary">
+            <i class="bi bi-funnel"></i> Filter
+        </button>
+    </div>
+</form>
 
 <x-ui.data-table
     id="suppliers-table"
-    :ajax-url="route('suppliers.data')"
+    :ajax-url="route('suppliers.data', request()->query())"
     :columns="[
         ['title' => '#', 'formatter' => 'rownum', 'hozAlign' => 'center', 'width' => 60],
         ['title' => 'Name', 'field' => 'name'],
